559a3157818f06c0df1666d30c2f9988de3790c4
Comcx <comcx@outlook.com>
2018-02-14 23:09:13 +0800

    Add files via upload

---
+++ lainlib.h
@@ -0,0 +1,93 @@
+
+#ifndef LAINLIB_H
+#define LAINLIB_H
+
+#define OK 1
+#ifndef ERROR
+	#define ERROR -1
+#endif
+#define STOP 0
+
+/**
+* Stream control tools
+* @author: C++ STL
+* @namespace: std
+*
+*/
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using std::cout;
+using std::cerr;
+using std::clog;
+using std::cin;
+using std::endl;
+
+/**
+* Containers
+* @author: C++ STL
+* @namespace: std
+* @include: vector, list, deque, string, stack, queue
+*			map, set
+*/
+#include <string>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+
+using std::string;
+
+/**
+* General algorithms
+* @author: C++ STL
+* @namespace: std
+*
+*/
+#include <algorithm>
+
+/**
+* Memory mangement
+* @author: C++ STL
+* @namespace: std
+*
+*/
+#include <memory>
+
+#include <functional>
+
+/**
+* Useful
+*
+*/
+#include <utility>
+#include <cctype>
+#include <cmath>
+#include <ctime>
+#include <windows.h>
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
